185d3f4049c5bd19468e2687f9125f3a6b5da444
Mokoena2000 <100692323+Mokoena2000@users.noreply.github.com>
2024-08-26 17:38:09 +0200

    completed varia

---
+++ ReviewExercises/variables.py
@@ -0,0 +1,12 @@
+#Create a program which stores a value in a variable and displays it
+
+def access():
+    if name == "moholeng":
+        return  (f"hello {name} welcome back")
+    
+    else:
+        return (f"hello {name} sorry go to own accouunt")
+    
+name = input("Enter your name: ")
+
+print(access())
